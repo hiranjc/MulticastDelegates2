3cc2fac4a591a0574f970f179c16e45179e280b7
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-30 18:51:09 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MulticastDelegates2.Services;
+
+namespace MulticastDelegates2
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowMax;
+            op += CalculationService.ShowSum;
+            op.Invoke(a, b);
+        }
+    }
+}
